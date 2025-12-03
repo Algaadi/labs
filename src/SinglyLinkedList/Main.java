d32493dabf367cd292f02b5b647eb96fb362e19e
abood <abood@LAPTOP-10DB6U41>
2025-12-03 21:23:47 +0300

    labs

---
+++ src/SinglyLinkedList/Main.java
@@ -0,0 +1,13 @@
+
+package SinglyLinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.addFirst(20);
+        list.addFirst(10);
+        list.display();
+        
+    }
+}
