7a747c3427ee947c537192d88a8e4856a85269ec
abood <abood@LAPTOP-10DB6U41>
2025-12-12 16:38:54 +0300

    labs2

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -1,13 +1,14 @@
-
 package SinglyLinkedList;
-
 
 public class Main {
     public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-        list.addFirst(20);
-        list.addFirst(10);
+        SinglyLinkedList<Integer> list= new SinglyLinkedList<Integer>();
+        list.addfirst(10);
+        list.addfirst(20);
+        list.addLast(50);
+        list.removeLast();
+        list.addAtPosition(80,3);
+        list.addLast(40);
         list.display();
-        
     }
 }
